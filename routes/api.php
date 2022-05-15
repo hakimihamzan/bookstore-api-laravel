3a478d78d45ae41cd2bdf3c8dc23f506d00ded47
kimi <hakimihamzan20@gmail.com>
2022-05-15 22:48:20 +0800

    halfway tutorial

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\AuthorsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('/test', function (Request $request) {
+//     return 'Authenticated';
+// });
+
+
+// authors/{author}
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Book belongs to authors
+// User that created books
+
+Route::middleware('auth:api')->prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        dd('hello');
+        return $request->user();
+    });
+
+    // Route::get('/authors/{author}', [AuthorsController::class, 'show']);
+
+    // Route::get('/authors', [AuthorsController::class, 'index']);
+
+    Route::apiResource('/authors', AuthorsController::class);
+});
